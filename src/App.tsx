f3a98d24d83ffc6afda8d9bb8895217567291757
Moisés <moisesnetored@gmail.com>
2022-09-06 09:04:02 -0400

    refactor: re-configuring eslint
    The old configuration did not have all the proper settings for the project, returning errors that
    were not supposed to be happening.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import reactLogo from './assets/react.svg';
 import './App.css';
 
 export function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <div>
@@ -17,9 +15,6 @@ export function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
